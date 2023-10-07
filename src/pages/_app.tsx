3bacf68a3eb454f32304158abea32ecff450fcae
EdgarSantiago <edgargsp777@gmail.com>
2023-10-07 02:23:45 -0300

    circulo pointer agora só segue em desktop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import Pointer from "@/components/global/pointer/Pointer";
 import Fonts from "@/lib/Fonts";
 import "@/styles/globals.css";
-import { CSSReset, ChakraProvider, theme } from "@chakra-ui/react";
+import { CSSReset, ChakraProvider, Show, theme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Pointer />
+      <Show above="lg">
+        <Pointer />
+      </Show>
       <Fonts />
       <CSSReset />
       <Component {...pageProps} />
